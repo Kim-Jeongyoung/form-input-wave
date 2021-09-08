ade13a4de7af08379fef5dc981a98966a62bfd91
Kim-Jeongyoung <jeongyoung2@gmail.com>
2021-09-08 13:05:43 -0700

    Adding wave effect (transition-delay & idx)

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const labels = document.querySelectorAll('.form-control label');
 labels.forEach((label) => {
   label.innerHTML = label.innerText
     .split('')
-    .map((letter, idx) => `<span>${letter}</span>`)
+    .map(
+      (letter, idx) =>
+        `<span style='transition-delay: ${idx * 50}ms'>${letter}</span>`
+    )
     .join('');
 });
+
+// transition delay에 dix를 넣고 ms(시간 ) 주기
