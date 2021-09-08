07b70094ddba6202e6b8b6e373b5192a7a40ad42
Kim-Jeongyoung <jeongyoung2@gmail.com>
2021-09-08 12:49:59 -0700

    To make labels to span

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+'use strict';
 
+const labels = document.querySelectorAll('.form-control label');
+
+//Email & Password 각각을  span에 넣어주기 (span default는 inline)
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map((letter, idx) => `<span>${letter}</span>`)
+    .join('');
+});
